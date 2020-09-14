d052324503f2eb1fe246a67f2646c03d914403b7
ghmeec <georgemillanzi1234@gmail.com>
2020-09-14 12:01:15 +0300

    components screen

--- app/screens/application-components-screen.tsx
+++ app/screens/application-components-screen.tsx
@@ -1,27 +1,19 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../components"
+import { Screen, Text } from "../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
-import { color } from "../../theme"
+import { color } from "../theme"
 
 const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
-  flex: 1,
+    backgroundColor: color.palette.black,
+    flex: 1,
 }
-
-export const ApplicationComponentsScreen = observer(function ApplicationComponentsScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
-  // OR
-  // const rootStore = useStores()
-
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
-  return (
-    <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="applicationComponentsScreen" />
-    </Screen>
-  )
-})
+export const ApplicationComponentsScreen = () => {
+    return (
+        <Screen style={ROOT} preset="scroll">
+            <Text preset="header" text="applicationComponentsScreen" />
+        </Screen>
+    )
+}
