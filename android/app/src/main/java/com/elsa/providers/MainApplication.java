bdcb3674410b8c918757d711e46ac63ee16f4f61
ally <3210jr@gmail.com>
2021-03-23 14:56:59 +0300

    upgrade attempt

--- android/app/src/main/java/com/elsa/providers/MainApplication.java
+++ android/app/src/main/java/com/elsa/providers/MainApplication.java
@@ -12,6 +12,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new ReanimatedJSIModulePackage());
           return packages;
         }
 
