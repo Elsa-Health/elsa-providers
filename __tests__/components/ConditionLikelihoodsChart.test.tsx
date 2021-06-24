e7b6ba4081a3301b1dfd48fe1f10025fd5f29200
iam-kevin <kevin.al.james@gmail.com>
2021-06-24 16:01:25 +0300

    attempt fix

--- __tests__/components/ConditionLikelihoodsChart.test.tsx
+++ __tests__/components/ConditionLikelihoodsChart.test.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-// import { render } from "@testing-library/react-native"
-// import { ConditionLikelihoodsChart } from "../../src/components/ConditionLikelihoodsChart"
+import { render } from "@testing-library/react-native"
+import { ConditionLikelihoodsChart } from "../../src/components/ConditionLikelihoodsChart"
 
 // jest.useFakeTimers();
 
 describe ("Conditional Likelihoods Chart", () => {
     test("Render check", () => {
-        expect(1).toBe(1)
-        // const {getByTestId} = render(<ConditionLikelihoodsChart data={[]}/>)
+        // expect(1).toBe(1)
+        const {getByTestId} = render(<ConditionLikelihoodsChart data={[]}/>)
 
-        // expect(getByTestId("ConditionLikelihoodsChart")).toBeDefined();
+        expect(getByTestId("ConditionLikelihoodsChart")).toBeDefined();
     })
 })
