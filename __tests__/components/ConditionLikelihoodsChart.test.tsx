e3315f63dba4f620b023951e259bbd70723f7b71
ally <3210jr@gmail.com>
2021-06-24 15:54:22 +0300

    updated test with parameters

--- __tests__/components/ConditionLikelihoodsChart.test.tsx
+++ __tests__/components/ConditionLikelihoodsChart.test.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react-native"
-import { ConditionLikelihoodsChart } from "../../src/components/ConditionLikelihoodsChart"
+import { render } from "@testing-library/react-native";
+import { ConditionLikelihoodsChart } from "../../src/components/ConditionLikelihoodsChart";
 
 jest.useFakeTimers();
 
-describe ("Conditional Likelihoods Chart", () => {
-    test("Render check", () => {
-        const {getByTestId} = render(<ConditionLikelihoodsChart data={[]}/>)
+describe("Conditional Likelihoods Chart", () => {
+	test("Render check", () => {
+		const { getByTestId } = render(
+			<ConditionLikelihoodsChart data={[{ y: 10, x: "Malaria" }]} />
+		);
 
-        expect(getByTestId("ConditionLikelihoodsChart")).toBeDefined();
-    })
-})
+		expect(getByTestId("ConditionLikelihoodsChart")).toBeDefined();
+	});
+});
