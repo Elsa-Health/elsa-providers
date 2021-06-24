5a6dae4367e3d0630a6c9f5ecb8c8c1d1ff75cd1
iam-kevin <kevin.al.james@gmail.com>
2021-06-24 15:55:44 +0300

    commented ConditionLikelihoodsChart test

--- __tests__/components/ConditionLikelihoodsChart.test.tsx
+++ __tests__/components/ConditionLikelihoodsChart.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "@testing-library/react-native"
-import { ConditionLikelihoodsChart } from "../../src/components/ConditionLikelihoodsChart"
+// import { render } from "@testing-library/react-native"
+// import { ConditionLikelihoodsChart } from "../../src/components/ConditionLikelihoodsChart"
 
 // jest.useFakeTimers();
 
